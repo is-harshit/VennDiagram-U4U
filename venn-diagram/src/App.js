ebe0177c0296d1b5763d3e085bb025b72b3707db
is-harshit <131351686+is-harshit@users.noreply.github.com>
2024-02-08 22:22:11 +0530

    Updated App.js

--- venn-diagram/src/App.js
+++ venn-diagram/src/App.js
@@ -1,24 +1,66 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import VennDiagram from './VennDiagram';
+// import {Chat} from 'stream-chat';
+import io from 'socket.io-client'
 import './App.css';
 import questions from './questions';
 
+const socket =io.connect("http://localhost:3001")
+
 const App = () => {
   const [player1Name, setPlayer1Name] = useState('');
-  const [player2Name, setPlayer2Name] = useState('');
-  const [player1Answers, setPlayer1Answers] = useState([]);
-  const [player2Answers, setPlayer2Answers] = useState([]);
+  const [playerAnswers, setPlayerAnswers] = useState([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [namesSubmitted, setNamesSubmitted] = useState(false);
   const [quizCompleted, setQuizCompleted] = useState(false);
+  const [roomId,setRoomId]=useState("");
+  const [playerReady,setPlayerReady]=useState(false);
+  // const [form,SetSubmitted]=useState(false); 
+  const [gameDone,setGameDone]=useState(false);
+  
+  // const [Final_answers,setFinalAnswers]=useState([]);
+
+  const [player1Answers, setPlayer1Answers] = useState([]);
+  const [player2Answers, setPlayer2Answers] = useState([]);
 
   const currentQuestion = questions[currentQuestionIndex];
 
+  const JoinGame = () =>{
+    // console.log("Why!");
+    if (player1Name !== ""  && roomId !=="" ){ //&& form){
+      socket.emit("join_room",roomId)
+      if (playerReady)
+      {
+        handleSubmitNames();
+      }
+    }
+  }
+
+  useEffect( ()=>{
+  socket.on("PlayersJoined", ()=>{
+    console.log("Received confirmation");
+    setPlayerReady(true);
+    handleSubmitNames();
+  })
+  },[socket]); 
+
+  useEffect( ()=>{
+    socket.on("GameOver", (answers_received)=>{
+      console.log("GameOver Received");
+      
+      // setPlayer1Answers(answers_received[0]);
+      // setPlayer2Answers(answers_received[1]);
+
+      // console.log(player1Answers,player2Answers);
+
+      setGameDone(true);
+
+    })
+    },[socket]); 
+
   const handleAnswerSelect = (player, answer) => {
     if (player === 1) {
-      setPlayer1Answers((prevAnswers) => [...prevAnswers, answer]);
-    } else {
-      setPlayer2Answers((prevAnswers) => [...prevAnswers, answer]);
+      setPlayerAnswers((prevAnswers) => [...prevAnswers, answer]);
     }
   };
 
@@ -28,6 +70,10 @@ const App = () => {
     } else {
       // Last question reached, mark the quiz as completed
       setQuizCompleted(true);
+
+      socket.emit("game_done",roomId);
+
+      // socket.emit("Answers",playerAnswers);
     }
   };
 
@@ -36,11 +82,10 @@ const App = () => {
     setNamesSubmitted(true);
   };
 
-  const sets = [
-    { sets: [player1Name], size: player1Answers.length },
-    { sets: [player2Name], size: player2Answers.length },
-    { sets: [player1Name, player2Name], size: player1Answers.filter((answer) => player2Answers.includes(answer)).length },
-  ];
+  // const sets = [
+  //   { sets: ['player1Name'], size: player1Answers.length },
+  //   { sets: ['player1Name', 'player2Name'], size: player1Answers.filter((answer) => player2Answers.includes(answer)).length },
+  // ];
 
   return (
     <div>
@@ -48,25 +93,30 @@ const App = () => {
 
       {!namesSubmitted && (
         <div className="player-names">
-          <label>Player 1 Name: </label>
+          <label>Player Name: </label>
           <input
             type="text"
             value={player1Name}
             onChange={(e) => setPlayer1Name(e.target.value)}
           />
-
-          <label>Player 2 Name: </label>
+          <br></br>
+          <label>Room ID: </label>
           <input
             type="text"
-            value={player2Name}
-            onChange={(e) => setPlayer2Name(e.target.value)}
+            value={roomId}
+            onChange={(e) => setRoomId(e.target.value)}
           />
 
-          <button onClick={handleSubmitNames}>Submit</button>
+          <button onClick={JoinGame}>Submit</button>
         </div>
+        
+        
       )}
 
+      
+
       {namesSubmitted && !quizCompleted && (
+        <client >
         <div>
           <h2>Question: {currentQuestion.text}</h2>
           <p>{player1Name}</p>
@@ -75,17 +125,11 @@ const App = () => {
               {option}
             </button>
           ))}
-          <p>Selected Option: {player1Answers[player1Answers.length - 1]}</p>
+          <p>Selected Option: {playerAnswers[playerAnswers.length - 1]}</p>
 
-          <p>{player2Name}</p>
-          {currentQuestion.options.map((option) => (
-            <button key={option} onClick={() => handleAnswerSelect(2, option)}>
-              {option}
-            </button>
-          ))}
-          <p>Selected Option: {player2Answers[player2Answers.length - 1]}</p>
         </div>
-      )}
+        </client>
+        )}
 
       {namesSubmitted && !quizCompleted && (
         <button onClick={handleNextQuestion}>Next</button>
@@ -94,22 +138,26 @@ const App = () => {
       {namesSubmitted && quizCompleted && (
         <div>
           <h2>{player1Name}'s Answers</h2>
-          <p>{player1Answers.join(', ')}</p>
-
-          <h2>{player2Name}'s Answers</h2>
-          <p>{player2Answers.join(', ')}</p>
-
+          <p>{playerAnswers.join(', ')}</p>
           
+          {namesSubmitted && quizCompleted && !gameDone &&(
+            <div>
+              <h2>Waiting for other person</h2>
+            </div>
+          )}
+
           {/* Compatibility Percentage */}
-          {namesSubmitted && quizCompleted && (
+          {namesSubmitted && quizCompleted && gameDone &&(
             <div>
               <h2>Compatibility Percentage</h2>
-              <p>{((sets[2].size / Math.min(sets[0].size, sets[1].size)) * 100).toFixed(2)}%</p>
-              <VennDiagram data={sets} />
+              {/* <p>{((sets[2].size / Math.min(sets[0].size, sets[1].size)) * 100).toFixed(2)}%</p>
+              <VennDiagram data={sets} /> */}
             </div>
           )}
         </div>
       )}
+
+      
     </div>
   );
 };
